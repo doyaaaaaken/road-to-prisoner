17af0ae4c1110322807ca3ef5a0bf7c8171968cd
doyaaaaaken <kenta.koyama.biz@gmail.com>
2020-07-07 23:33:58 +0900

    floating button

--- app/src/main/java/org/doyaaaaaken/prisonertraining/RecordListActivity.kt
+++ app/src/main/java/org/doyaaaaaken/prisonertraining/RecordListActivity.kt
@@ -1,11 +1,18 @@
 package org.doyaaaaaken.prisonertraining
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RecordListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_list)
+    }
+
+    fun onClickRecordCreateButton(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
